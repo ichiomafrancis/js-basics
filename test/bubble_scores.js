99e9b0310e99bf7f24a5a60bf376deab09e1695d
Francis Chioma <ichiomafrancis@gmail.com>
2022-09-28 16:26:41 +0100

    added the costs data

--- test/bubble_scores.js
+++ test/bubble_scores.js
@@ -1,6 +1,12 @@
-var scores = [
+let scores = [
   60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69,
   64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44,
+];
+
+let costs = [
+  0.25, 0.27, 0.25, 0.25, 0.25, 0.25, 0.33, 0.31, 0.25, 0.29, 0.27, 0.22, 0.31,
+  0.25, 0.25, 0.33, 0.21, 0.25, 0.25, 0.25, 0.28, 0.25, 0.24, 0.22, 0.2, 0.25,
+  0.3, 0.25, 0.24, 0.25, 0.25, 0.25, 0.27, 0.25, 0.26, 0.29,
 ];
 
 // console.log(scores.length);
@@ -25,6 +31,17 @@ function getMaxIndexes(scores, maxScore) {
     }
   }
   return maxIndexes;
+}
+
+function getMostCostEffectiveSolution(scores, costs, maxScore) {
+  let cost = 100;
+  let index;
+  for (let i = 0; i <= scores.length - 1; i++) {
+    if (scores[i] == maxScore)
+      if (cost > costs[i]) {
+        index = i;
+      }
+  }
 }
 
 let maxScore = printAndGetMaxScore(scores);
