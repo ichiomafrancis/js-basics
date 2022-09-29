85b77b2224c5c4a5d28757ac471e84c197472a4f
Francis Chioma <ichiomafrancis@gmail.com>
2022-09-29 13:23:33 +0100

    iterated through the scores array and compared with the costs array to find the most cost effective solution

--- test/bubble_scores.js
+++ test/bubble_scores.js
@@ -9,7 +9,6 @@ let costs = [
   0.3, 0.25, 0.24, 0.25, 0.25, 0.25, 0.27, 0.25, 0.26, 0.29,
 ];
 
-// console.log(scores.length);
 function printAndGetMaxScore(scores) {
   let output;
   let maxScore = 0;
@@ -34,37 +33,61 @@ function getMaxIndexes(scores, maxScore) {
 }
 
 function getMostCostEffectiveSolution(scores, costs, maxScore) {
-  let cost = 100;
+  let cost = 100; // much higher than any of the costs
   let index;
+
   for (let i = 0; i <= scores.length - 1; i++) {
-    if (scores[i] == maxScore)
+    if (scores[i] == maxScore) {
       if (cost > costs[i]) {
         index = i;
+        cost = costs[i];
       }
+    }
   }
+  return index;
 }
 
+// //
+// // Another way to write this is to use the bestSolutions array,
+// // and use the index stored there to find the cost value of that solution.
+// // This is a little more efficient, but not quite as easy to read!
+// //
+// function getMostCostEffectiveSolution2(bestSolutions, costs) {
+// 	var cost = 100;
+// 	var solutionIndex;
+// 	var lowCostIndex;
+
+// 	for (var i = 0; i < bestSolutions.length; i++) {
+// 		solutionIndex = bestSolutions[i];
+// 		if (cost > costs[solutionIndex]) {
+// 			lowCostIndex = solutionIndex;
+// 			cost = costs[solutionIndex];
+// 		}
+// 	}
+// 	return lowCostIndex;
+// }
+
+//
+// compute the highest score and display results
+//
 let maxScore = printAndGetMaxScore(scores);
 console.log("Bubbles tests: " + scores.length);
 console.log("Highest bubble score: " + maxScore);
 
+//
+// compute the best solutions and display
+//
 let maxIndexes = getMaxIndexes(scores, maxScore);
 console.log("Solutions with highest score: " + maxIndexes);
 
-// Similiar solution using while loop
+//
+// compute the most cost effective of the best solutions
+//
+let mostCostEffective = getMostCostEffectiveSolution(scores, costs, maxScore);
+console.log(
+  "Bubble Solution #" + mostCostEffective + " is the most cost effective"
+);
 
-// var output;
-//  var i = 0;
-//  while (i < scores.length) {
-//  output = "Bubble solution #" + i + " score: " + scores[i];
-//  console.log(output);
-//  i = i + 1;
-// console.log(output);
-// if (scores[i] > max) {
-//   max = scores[i];
-// }
-// }
-// console.log("Bubbles tests: " + scores.length);
-// console.log("Highest bubble score: " + max);
-
-// console.log("Solutions with highest score: " + "#" + highestScore);
+// // or use the more efficient function:
+// mostCostEffective = getMostCostEffectiveSolution2(bestSolutions, costs);
+// console.log("Bubble Solution #" + mostCostEffective + " is the most cost effective");
