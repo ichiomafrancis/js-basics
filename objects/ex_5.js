f9905065259ebf17c4291749f84d9896bdd19e79
Francis Chioma <ichiomafrancis@gmail.com>
2022-10-21 16:45:14 +0100

    Started ex 5 assignment of the object topic

---
+++ objects/ex_5.js
@@ -0,0 +1,28 @@
+let programming = {
+  languages: ["JavaScript", "Python", "Ruby"],
+  isChallenging: true,
+  isRewarding: true,
+  difficulty: 8,
+  jokes:
+    "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke",
+};
+
+programming.languages.push(go);
+
+programming.difficulty = 7;
+
+// delete.programming.jokes;
+
+programming.isFun = true;
+
+for (let language of programming.languages) {
+  console.log(language);
+}
+
+for (let key in programming) {
+  console.log(key);
+}
+
+for (let key in programming) {
+  console.log(programming[key]);
+}
