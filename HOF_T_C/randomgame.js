89ad433c4f7e69b8ceeb207ed75dcc1f97693ced
Francis Chioma <ichiomafrancis@gmail.com>
2022-11-10 04:13:21 +0100

    Completed the randomgame exercise of the Higher order function, timer and closure topic

--- HOF_T_C/randomgame.js
+++ HOF_T_C/randomgame.js
@@ -3,3 +3,23 @@
 Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds and each time that a random number is picked, add 1 to a counter. If the number is greater than .75, stop the timer and return the number of tries it took before we found a number greater than .75.
 
 */
+
+function randomGame() {
+  let number;
+  let count = 0;
+  let intervalID = setInterval(function () {
+    number = Math.random();
+    count += 1;
+    if (number > 0.75) {
+      clearInterval(intervalID);
+      console.log(number);
+      console.log(
+        'It took ' +
+          count +
+          ' try/tries to find a random number greater than 0.75'
+      );
+    }
+  }, 1000);
+}
+
+randomGame();
